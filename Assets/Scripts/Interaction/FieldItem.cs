97f6f4a37f3658409a028e5ab2f6a738a1611078
DaeHan Jang <abouttt57@gmail.com>
2024-07-10 06:45:37 +0900

    필드 아이템 추가

---
+++ Assets/Scripts/Interaction/FieldItem.cs
@@ -0,0 +1,70 @@
+using System.Collections.Generic;
+using UnityEngine;
+using AYellowpaper.SerializedCollections;
+
+public class FieldItem : Interactable
+{
+    public IReadOnlyDictionary<ItemData, int> Items => _items;
+
+    [SerializeField]
+    private bool _destroyWhenEmpty = true;
+
+    [SerializeField]
+    private SerializedDictionary<ItemData, int> _items;
+
+    private void Start()
+    {
+        if ((_items == null || _items.Count == 0) && _destroyWhenEmpty)
+        {
+            Managers.Resource.Destroy(gameObject);
+        }
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        foreach (var item in Items)
+        {
+            Player.ItemInventory.AddItem(item.Key, item.Value);
+        }
+    }
+
+    public void AddItem(ItemData itemData, int count)
+    {
+        if (count <= 0)
+        {
+            return;
+        }
+
+        if (!_items.ContainsKey(itemData))
+        {
+            _items.Add(itemData, 0);
+        }
+
+        _items[itemData] += count;
+    }
+
+    public virtual void RemoveItem(ItemData itemData, int count)
+    {
+        if (count <= 0)
+        {
+            return;
+        }
+
+        if (_items.ContainsKey(itemData))
+        {
+            _items[itemData] -= count;
+
+            if (_items[itemData] <= 0)
+            {
+                _items.Remove(itemData);
+            }
+        }
+
+        if (_items.Count == 0 && _destroyWhenEmpty)
+        {
+            Managers.Resource.Destroy(gameObject);
+        }
+    }
+}
